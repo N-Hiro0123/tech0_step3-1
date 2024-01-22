1eefa94aabf4564c9ce9b26f7c1649a2c0004b84
N-Hiro0123 <n.hiroki0909@gmail.com>
2024-01-23 01:18:51 +0900

    streamlit用のmain_front.pyを実装。DB作成についてもmain_back.pyで作り直した。DBについてもテーブルを増やした

---
+++ main_back.py
@@ -0,0 +1,347 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+from tqdm import tqdm   #for文の進捗を確認
+#セットアップ
+tqdm.pandas()  
+
+# googleスプレッドシートの編集
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import json
+
+# 地図表示するために緯度、経度情報も取得しておく
+import requests
+import urllib
+
+# SQLiteを実施
+import sqlite3
+
+# 環境変数の設定
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+SP_CREDENTIAL_FILE = os.getenv('SP_CREDENTIAL_FILE')
+SP_COPE = [
+    'https://www.googleapis.com/auth/drive',
+    'https://spreadsheets.google.com/feeds'
+]
+SP_SHEET_KEY = os.getenv('SP_SHEET_KEY')
+SP_SHEET = 'meguro_2K2DK2LDK'
+
+
+def scraping_suumo(load_url: str) -> pd.DataFrame:
+    """suumoページからスクレイピング
+
+    Args:
+        load_url (str): エリア、間取りなどを検索した後で「建物ごとに表示」のurl
+
+    Returns:
+        pd.DataFrame:コラム名は "名前", "住所", "最寄り駅１","最寄り駅２","最寄り駅３", "築年数", "建物全体の階数", "階数", "賃料", "管理費", "敷金", "礼金", "間取り", "面積", "URL" 
+    """
+
+    res = requests.get(load_url)
+    res.encoding = "utf-8"
+    soup = BeautifulSoup(res.text, "html.parser")
+    
+    # ページ数の取得
+    pages = soup.find("ol", class_="pagination-parts")
+    num_of_pages = int(pages.find_all("li")[-1].text)
+    
+    
+    # データフレーム作成
+    rental_property_datas = pd.DataFrame(columns=["名前", "住所", "最寄り駅１","最寄り駅２","最寄り駅３", "築年数", "建物全体の階数", "階数", "賃料", "管理費", "敷金", "礼金", "間取り", "面積", "URL" ])
+    
+    print("suumo scraping")
+    # for p in tqdm(range(num_of_pages)):
+    for p in tqdm(range(2)):  # テストの際に使用
+
+        # topのページは"&page=1"を付けても開ける
+        page_url = load_url + "&page=" + str(p+1)
+        res = requests.get(page_url)
+        res.encoding = "utf-8"
+        soup = BeautifulSoup(res.text, "html.parser")
+        
+        # サーバー負荷を避けるため1s遅延
+        time.sleep(1)
+
+        # 「建物ごとに表示」ページから物件毎の情報を取得
+        cassetitems = soup.find_all("div", class_="cassetteitem")
+
+        for i in range(len(cassetitems)):
+            
+            # 上の部分（建物情報）
+            details = cassetitems[i].find_all("div", class_="cassetteitem-detail")
+            
+            for ii in range(len(details)):
+                name = details[ii].find("div", class_="cassetteitem_content-title").text
+                address = details[ii].find("li", class_="cassetteitem_detail-col1").text
+                _stations = details[ii].find_all("div", class_="cassetteitem_detail-text")
+                station1 = _stations[0].text
+                station2 = _stations[1].text
+                station3 = _stations[2].text
+                _col3 = details[ii].find_all("li", class_="cassetteitem_detail-col3")
+                building_age = _col3[0].find_all("div")[0].text
+                number_of_floors = _col3[0].find_all("div")[1].text
+                
+                # 下の部分（部屋の情報）
+                items = cassetitems[i].find("div", class_="cassetteitem-item")
+                tbodys = items.find_all("tbody")
+                for iii in range(len(tbodys)):
+                    _tds = tbodys[iii].find_all("td")
+                    floor = _tds[2].text.replace('\r','').replace('\n','').replace('\t','')
+                    rent = _tds[3].find("span", class_="cassetteitem_other-emphasis ui-text--bold").text
+                    maintenance_fee = _tds[3].find("span", class_="cassetteitem_price cassetteitem_price--administration").text
+                    deposit = _tds[4].find("span", class_="cassetteitem_price cassetteitem_price--deposit").text
+                    gratuity = _tds[4].find("span", class_="cassetteitem_price cassetteitem_price--gratuity").text
+                    layout = _tds[5].find("span", class_="cassetteitem_madori").text
+                    area = _tds[5].find("span", class_="cassetteitem_menseki").text
+                    url = "https://suumo.jp" + _tds[8].find("a").get("href")
+                    
+                    # DataFrameにまとめた後、rental_property_datasへ追加する
+                    _d = pd.DataFrame()
+                    _d["名前"] = [name]
+                    _d["住所"] = [address]
+                    _d["最寄り駅１"] = [station1]
+                    _d["最寄り駅２"] = [station2]
+                    _d["最寄り駅３"] = [station3]
+                    _d["築年数"] = [building_age]
+                    _d["建物全体の階数"] = [number_of_floors]
+                    _d["階数"] = [floor]
+                    _d["賃料"] = [rent]
+                    _d["管理費"] = [maintenance_fee]
+                    _d["敷金"] = [deposit]
+                    _d["礼金"] = [gratuity]
+                    _d["間取り"] = [layout]
+                    _d["面積"] = [area]
+                    _d["URL"] = [url]
+
+                    rental_property_datas = pd.concat([rental_property_datas, _d], ignore_index=True)
+
+    return rental_property_datas
+    
+
+# クレンジングに使用した処理を関数化
+def replace_B_to_minus(floor: str) -> int:
+    """地下を示すBを"-"に置き換える（"階数"で使用）
+
+    Args:
+        floor (str): X階数の"X"だけを渡す_
+
+    Returns:
+        int: 特に、BX -> -X で返す
+    """
+    if "B" in floor :
+        floor = int(floor.replace("B","-"))
+    else :
+        floor = int(floor)
+
+    return floor
+
+def replace_man_yen_to_int(money: str) -> float:
+    """X.X万円をfloatに変換する
+
+    Args:
+        money (str): X.X万円
+
+    Returns:
+        float: X.X (万円)
+    """
+    if "-" in money :
+        money = float(0.0)
+    else:
+        money = float(money.replace("万円", ""))
+
+    return money
+
+def replace_yen_to_int(money: str) -> float:
+    """X000円を0.X万円に変換する
+
+    Args:
+        money (str): X000円
+
+    Returns:
+        float: 0.X (万円)
+    """
+    if "-" in money :
+        money = float(0)
+    else :
+        money = float(money.replace("円", "")) * 0.0001
+
+    return money
+
+def cleansing_suumo(rental_property_datas: pd.DataFrame) -> pd.DataFrame:
+    """suumoからスクレイピング後のデータをクレンジングと重複削除
+
+    Args:
+        rental_property_datas (pd.DataFrame): スクレイピング後のDataFrameを直接渡せばよい
+
+    Returns:
+        pd.DataFrame: 
+    """
+    
+    # if文で作成してたものをラムダ式に整理
+
+    rental_property_datas["築年数"] = rental_property_datas["築年数"].apply(lambda x : int(0) if x=="新築" else int(x.replace("築","").replace("年","")))
+
+    # "建物全体の階数を"最小と最大のコラムに直す
+    rental_property_datas["建物階数_最小"] = rental_property_datas["建物全体の階数"].apply(lambda x : int(x.split("地上")[0].replace("地下","-")) if "地下" in x else int(0))
+    rental_property_datas["建物階数_最大"] = rental_property_datas["建物全体の階数"].apply(lambda x : int(x.split("地上")[1].replace("階建","")) if "地下" in x else int(x.replace("階建","")))
+
+    # "階数"を"最小と最大のコラムに直す
+    rental_property_datas["階数_最小"] = rental_property_datas["階数"].apply(lambda x : replace_B_to_minus(x.split("-")[0]) if "-" in x else replace_B_to_minus(x.replace("階", "")))
+    rental_property_datas["階数_最大"] = rental_property_datas["階数"].apply(lambda x : replace_B_to_minus(x.split("-")[1].replace("階", "")) if "-" in x else replace_B_to_minus(x.replace("階", "")))
+
+    rental_property_datas["賃料"] = rental_property_datas["賃料"].apply(lambda x: replace_man_yen_to_int(x))
+    rental_property_datas["敷金"] = rental_property_datas["敷金"].apply(lambda x : replace_man_yen_to_int(x))
+    rental_property_datas["礼金"] = rental_property_datas["礼金"].apply(lambda x : replace_man_yen_to_int(x))
+
+    rental_property_datas["管理費"] = rental_property_datas["管理費"].apply(lambda x : replace_yen_to_int(x))
+
+    rental_property_datas["面積"] = rental_property_datas["面積"].apply(lambda x : float(x.replace("m2","")))
+    # rental_property_datas["間取り"] = rental_property_datas["間取り"]  # そのままでよい
+    
+    
+    # 重複判定の実施
+    df = rental_property_datas
+    df_cleansing= df[df[["住所","賃料","管理費","間取り","建物階数_最小","建物階数_最大","階数_最小","階数_最大","面積","敷金","礼金"]].duplicated()]
+    
+    return df_cleansing
+
+
+def get_logitude_and_latitude(address:str)  :
+    """住所から緯度、経度を取得
+
+    Args:
+        address (str): _description_
+        float (_type_): _description_
+    """
+    makeUrl = "https://msearch.gsi.go.jp/address-search/AddressSearch?q="
+    s_quote = urllib.parse.quote(address)
+    response = requests.get(makeUrl + s_quote)
+    logitude = response.json()[0]["geometry"]["coordinates"][1]  # 緯度 ※順番注意！
+    latitude = response.json()[0]["geometry"]["coordinates"][0]  # 経度
+    
+    # サーバー負荷を避けるため0.2s遅延
+    time.sleep(0.2)
+    
+    return (logitude, latitude)
+
+def add_logitude_and_latitude(df_cleansing: pd.DataFrame) -> pd.DataFrame :
+    """クレンジング後のDataFrameに緯度、経度のコラムを追加
+
+    Args:
+        df_cleansing (pd.DataFrame): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+
+    # DBへ格納するためにはlistはダメ
+    # apply()の引数として、result_type="expand"が使えなかったので、apply(pd.Series)で分割     
+    print("add loagitude and latitude")
+    _df = df_cleansing["住所"].progress_apply(lambda x : get_logitude_and_latitude(x)).apply(pd.Series) 
+    _df.columns = ["緯度", "経度"]
+    df_cleansing.loc[:, ["緯度", "経度"]] = _df
+
+    # SettingWithCopyWarning（ビューへ操作なのか、コピーへの操作なのか不明瞭）と言われるのでやめた
+    # df_cleansing[["緯度", "経度"]] = df_cleansing["住所"].progress_apply(lambda x : get_logitude_and_latitude(x)).apply(pd.Series) 
+    
+    return df_cleansing
+    
+def save_to_db(dbname: str, tablename:str, df_cleansing: pd.DataFrame) -> None :
+    """dfをDBへ保存する
+
+    Args:
+        dbname (str): _description_
+        tablename (str): _description_
+        df_cleansing (pd.DataFrame): _description_
+    """
+
+    # データベースに接続
+    conn = sqlite3.connect(dbname)  # 渡した名前でデータベースを作成（すでに存在していれば、それに接続する）
+
+    # DataFrameはsqliteに保存するメソッドが存在している
+    df_cleansing.to_sql(tablename, conn, if_exists='replace',  index=False)
+
+    # 接続を閉じる（必須）
+    conn.close()
+
+    return 
+
+def add_DB_list(df_DB_list: pd.DataFrame, load_url: str, dbname: str, tablename: str) -> pd.DataFrame :
+    """スクレイピング先と作成するDBをdf_DB_listへ追加する
+
+    Args:
+        df_DB_list (pd.DataFrame): _description_
+        load_url (str): _description_
+        dbname (str): _description_
+        tablename (str): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    _df = pd.DataFrame()
+    _df["load_url"] = [load_url]
+    _df["dbname"] = [dbname]
+    _df["tablename"] = [tablename]
+    
+    df_DB_list = pd.concat([df_DB_list, _df], ignore_index=True)
+    
+    return df_DB_list
+
+
+def main():
+    
+    # スクレイピング先と作成するDBの一覧
+    df_DB_list = pd.DataFrame()
+    
+    # 目黒区、2K, 2DK, 2LDK （建物種別　マンション、アパート）
+    df_DB_list = add_DB_list(
+        df_DB_list,
+        load_url ="https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13110&cb=0.0&ct=9999999&et=9999999&md=05&md=06&md=07&ts=1&ts=2&cn=9999999&mb=0&mt=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&fw2=",
+        dbname ="Chintai.db",
+        tablename="meguro_2K2DK2LDK",
+    )
+    
+    # 世田谷区、2K, 2DK, 2LDK （建物種別　マンション、アパート）
+    df_DB_list = add_DB_list(
+        df_DB_list,
+        load_url ="https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13112&cb=0.0&ct=9999999&et=9999999&md=05&md=06&md=07&ts=1&ts=2&cn=9999999&mb=0&mt=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&fw2=&srch_navi=1",
+        dbname ="Chintai.db",
+        tablename="setagaya_2K2DK2LDK",
+    )
+    
+    # 品川区、2K, 2DK, 2LDK （建物種別　マンション、アパート）
+    df_DB_list = add_DB_list(
+        df_DB_list,
+        load_url ="https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13109&cb=0.0&ct=9999999&et=9999999&md=05&md=06&md=07&ts=1&ts=2&cn=9999999&mb=0&mt=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&fw2=&srch_navi=1",
+        dbname ="Chintai.db",
+        tablename="shinagawa_2K2DK2LDK",
+    )
+    
+    # 港区、2K, 2DK, 2LDK （建物種別　マンション、アパート）
+    df_DB_list = add_DB_list(
+        df_DB_list,
+        load_url ="https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13103&cb=0.0&ct=9999999&et=9999999&md=05&md=06&md=07&ts=1&ts=2&cn=9999999&mb=0&mt=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&fw2=&srch_navi=1",
+        dbname ="Chintai.db",
+        tablename="minato_2K2DK2LDK",
+    )
+    
+    for i in range(len(df_DB_list)):
+
+        print(df_DB_list["load_url"][i])
+        print(df_DB_list["dbname"][i])
+        print(df_DB_list["tablename"][i])
+        
+        df_scraping = scraping_suumo(df_DB_list["load_url"][i])  # スクレイピング
+        df_cleansing = cleansing_suumo(df_scraping)  # クレンジング
+        df_cleansing = add_logitude_and_latitude(df_cleansing)  # 緯度・経度の追加
+        save_to_db(df_DB_list["dbname"][i], df_DB_list["tablename"][i], df_cleansing)
+    
+
+
+if __name__ == '__main__':
+    main()
--- main_front.py
+++ main_front.py
@@ -8,15 +8,19 @@ import sqlite3
 import folium
 from streamlit_folium import st_folium
 
+import os
+
 set_madori_list = {
     "2K" : "2K",
     "2DK" : "2DK",
-    "2LDK" : "2LDK"
+    "2LDK" : "2LDK",
 }
 
 set_erea_list = {
-    "目黒区" : "megro",
-    "渋谷区" : "shibuya",
+    "世田谷区" : "setagaya",
+    "目黒区" : "meguro",
+    "品川" : "shinagawa",
+    "港区" : "minato",
 }
 
 set_display_number_list = {
@@ -26,92 +30,211 @@ set_display_number_list = {
 }
 
 # 保存したdbの読み込んで確認
-dbname = "Chintai_test.db"  # テスト中
-tablename = "meguro_2K2DK2LDK"
+dbname = "Chintai.db"
 
 
-#<条件設定> キーワード・ソースのデフォルト値を設定
-if 'set_madori' not in st.session_state:
-    st.session_state.set_madori = "2LDK"
-    st.session_state.set_erea = "目黒区"
+def read_db(dbname: str, erea_list: list) -> pd.DataFrame:
+    """選択したエリアについてDBから読み込む
 
-st.title("賃貸検索アプリ")
-st.session_state.set_madori = st.sidebar.multiselect("間取りを選択", set_madori_list.keys())
-st.session_state.set_erea = st.sidebar.multiselect("エリアを選択", set_erea_list.keys())
+    Args:
+        dbname (str): 現状は"Chintai.db"で固定
+        set_erea_list (str): st.multiselectの戻り値をそのままいれればよい
 
+    Returns:
+        pd.DataFrame: 読み込んだDataFrameをすべて結合したもの
+    """
 
-value = st.sidebar.slider("価格範囲を指定してください[万円]", 0.0, 50.0, (20.0,30.0))
-
-st.write(st.session_state.set_madori[0])
-st.write(st.session_state.set_erea[0])
-st.write(set_erea_list[st.session_state.set_erea[0]])
-
-st.write(str(value[0])+"万円～"+str(value[1])+"万円")
-
-# 検索条件からクエリを作成する
-
-
-# DBは区毎に作成し、各DBに対して指定したクエリを投げる関数を作成
-# データベースに接続
-conn = sqlite3.connect(dbname)  # 渡した名前でデータベースを作成（すでに存在していれば、それに接続する）
-
-# SQLクエリを使用してデータの読み込んで、DataFrameへ変換
-query = "SELECT * FROM " + tablename
-df_read = pd.read_sql_query(query, conn)
-
-# 接続を閉じる（必須）
-conn.close()
-
-# 複数選択可能なselectbox形式
-select_list = [False, False, False, False, False, False, False, False, False, False]
-for i in range(10):
-    select_list[i] = st.checkbox(str(i))
-
-st.write(df_read.head(10))
-selected_one = st.radio("選択してください", df_read.head(10).index, horizontal=True)
-
-
-logitude = df_read.head(10)["緯度"].mean()
-latitude = df_read.head(10)["経度"].mean()
-
-center_location = [logitude, latitude]
-
-map = folium.Map(location=center_location, zoom_start=13)   # 全部の検索結果が入るサイズにしたいが、実スケールとの対応が分からない。ディスプレイの解像度などにも依存しそう。。。
-
-for i in range(10):
-    logitude = df_read.iloc[i]["緯度"]
-    latitude = df_read.iloc[i]["経度"]
-    name = df_read.iloc[i]["名前"]
+    # 現在のファイルのディレクトリを取得
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    # データベースファイルへのパスを構築
+    db_path = os.path.join(current_dir, dbname)
     
-    folium.Marker(
-        location = [logitude, latitude],
-        popup = name,
-        icon = folium.Icon(color="blue"),
-        ).add_to(map)
+    # データベースに接続
+    conn = sqlite3.connect(db_path)  # 渡した名前でデータベースを作成（すでに存在していれば、それに接続する）
+    df_read = pd.DataFrame()
 
+    # エリアごとにtable分けているので順番に読み込む
+    for i in range(len(erea_list)):
+        tablename = set_erea_list[erea_list[i]] + "_2K2DK2LDK"
+        # SQLクエリを使用してデータの読み込んで、DataFrameへ変換
+        query = "SELECT * FROM " + tablename
+        _df = pd.read_sql_query(query, conn)
+        
+        df_read = pd.concat([df_read, _df], ignore_index=True)
 
-# st.data_editorで直接チェックボックスを編集させてもよいかも。。。
-# for i in range(len(select_list)):
-#     if select_list[i] :
-#         logitude = df_read.iloc[i]["緯度"]
-#         latitude = df_read.iloc[i]["経度"]
-#         name = df_read.iloc[i]["名前"]
+    # 接続を閉じる（必須）
+    conn.close()
+    
+    return df_read
 
-#         folium.Marker(
-#             location = [logitude, latitude],
-#             popup = name,
-#             icon = folium.Icon(color="red"),
-#             ).add_to(map)
+def filter_df(df: pd.DataFrame, set_madori_value: list, rent_lower_and_upper_limits: list) -> pd.DataFrame:
+    """指定した条件を満たすものを抽出する
+
+    Args:
+        df_read (pd.DataFrame): 読み込んだ全データ
+        set_madori (list): 選択された"間取り"のvalueのリスト
+        value (list): "賃料"が[下限, 上限]に入るものを抽出する
+
+    Returns:
+        pd.DataFrame: 抽出結果のdf（インデックスを振りなおしたもの）
+    """
+
+    df = df[df["間取り"].isin(set_madori_value)]
+    df = df[df["賃料"].between(rent_lower_and_upper_limits[0], rent_lower_and_upper_limits[1])]
+    
+    # 抽出したデータをソートして、indexが０から始まるように付け直す
+    # この際に元のdfを変更したくないのでcopy()を作成する
+    df = df.sort_values("賃料", ascending=True)
+    df_copy = df.copy().reset_index(drop=True)
+
+    return df_copy
+
+# 表示する項目、数などの指定
+def display_df(filtered_df: pd.DataFrame, display_number: int, display_page: int) -> pd.DataFrame:
+    """条件で抽出した後のデータから、更に、表示するデータのみを抽出する
+
+    Args:
+        filtered_df (pd.DataFrame): 条件抽出後のデータでindex_resetしたものを想定
+        display_number (int): 一度に表示する物件の数
+        display_page (int): 表示するページ
+
+    Returns:
+        pd.DataFrame: 指定した１ページ分に相当するデータ
+    """
+   
+    # 一度に表示する件数、表示するページをもとに、対応する番号のデータを抽出
+    df = filtered_df[(display_page-1) * display_number : display_page * display_number]
+    
+    return df
+
+def display_map(filtered_df: pd.DataFrame, displayed_df: pd.DataFrame, selected_one: int) -> None:
+    """foliumを使って地図を表示する
+
+    Args:
+        filtered_df (pd.DataFrame): 条件で抽出済みdf(インデックスを振りなおしたもの)
+        displayed_df (pd.DataFrame): 一覧として表示するdf
+        selected_one (int): 強調したいデータのindex(filtered_dfに対するindex)
+    
+    Notes:
+        一覧として表示した物件すべて地図上にプロットする。
+        その上で選択した物件についてはマーカーの色を変える。
+    """
+
+    # 地図の中心は、表示する物件の中央付近にしたい
+    logitude = displayed_df["緯度"].mean()
+    latitude = displayed_df["経度"].mean()
+    
+    center_location = [logitude, latitude]
+
+    # 全部の検索結果が入るサイズにしたいが、実スケールとの対応が分からない。ディスプレイの解像度などにも依存しそう。。
+    # 自分の環境でちょうど良さそうなzoom_start=12としておく
+    map = folium.Map(location=center_location, zoom_start=12)
+
+    # 一覧表示の物件をすべて地図上にプロットする
+    for i in displayed_df.index:
+        # 参照するindexはfilterd_dfに対応している
+        logitude = filtered_df.iloc[i]["緯度"]  
+        latitude = filtered_df.iloc[i]["経度"]
+        name = filtered_df.iloc[i]["名前"]
+        
+        folium.Marker(
+            location = [logitude, latitude],
+            popup = name,
+            icon = folium.Icon(color="blue"),
+            ).add_to(map)
+
+    # 選択した物件のみマーカーを変える
     i = selected_one
-    logitude = df_read.iloc[i]["緯度"]
-    latitude = df_read.iloc[i]["経度"]
-    name = df_read.iloc[i]["名前"]
+    logitude = filtered_df.iloc[i]["緯度"]
+    latitude = filtered_df.iloc[i]["経度"]
+    name = filtered_df.iloc[i]["名前"]
 
     folium.Marker(
         location = [logitude, latitude],
         popup = name,
         icon = folium.Icon(color="red"),
         ).add_to(map)
+    
+    # 地図の表示
+    st_folium(map, width=700, height=500)    
+    
+    return
+
+#<条件設定> キーワード・ソースのデフォルト値を設定
+if 'set_madori' not in st.session_state:
+    st.session_state.set_madori = "2LDK"
+    st.session_state.set_erea = "目黒区"
+    st.session_state.rent_lower_and_upper_limits = (20.0, 30.0)
+    st.session_state.display_number = "10件"
+    st.session_state.filtered_df = pd.DataFrame()
+    st.session_state.search_button_is_not_pushed = True  # 検索ボタンを一度でも押せばFalseになる
+
+st.title("賃貸検索アプリ")
+st.session_state.set_madori = st.sidebar.multiselect("間取りを選択", set_madori_list.keys(), st.session_state.set_madori)
+st.session_state.set_erea = st.sidebar.multiselect("エリアを選択", set_erea_list.keys(), st.session_state.set_erea)
+st.session_state.rent_lower_and_upper_limits = st.sidebar.slider("価格範囲を指定してください[万円]", 0.0, 50.0, st.session_state.rent_lower_and_upper_limits)
+
+# keyのリストをvalueのリストに変換する
+set_madori_value = []
+for i in range(len(st.session_state.set_madori)):
+    set_madori_value.append(set_madori_list[st.session_state.set_madori[i]])
 
 
-st_folium(map, width=700, height=500)
+# DBの読み込み
+df_read = read_db(dbname, st.session_state.set_erea)
+
+# 検索ボタンを押したときのみ
+if st.sidebar.button("検索", type="primary"):
+    # 指定した条件のデータを抽出する
+    st.session_state.filtered_df = filter_df(df_read, set_madori_value, st.session_state.rent_lower_and_upper_limits)
+    
+    st.session_state.search_button_is_not_pushed = False
+
+
+# 検索ボタンが押されるまではこちらの処理 
+if st.session_state.search_button_is_not_pushed :
+    st.write("条件を指定した上で検索ボタンを押してください")
+    
+# 検索ボタンが押された時のみ（filterd_dfにデータが入っている時のみ）実行する   
+else :
+    # 検索結果の表示
+    st.sidebar.write("条件に合う物件は" + str(len(st.session_state.filtered_df)) + "件です")
+    
+    # 検索結果が０件の時の例外処理
+    if len(st.session_state.filtered_df) == 0 :
+        st.write("条件を変えて再度検索をしてください")
+    
+    # 検索結果が１件以上ある時の通常処理
+    else :
+        # 表示する条件を指定する
+        # 一度に表示する物件の数を指定
+        st.session_state.display_number_key = st.sidebar.radio("一度に表示する件数",set_display_number_list.keys())
+        st.session_state.display_number = set_display_number_list[st.session_state.display_number_key]
+
+        # 表示するページ番号の指定
+        max_page = len(st.session_state.filtered_df) // st.session_state.display_number + 1
+
+        # st.sliderでは最小値＜最大値でないとエラーがでるため、max_page==1の時はsliderを使わない
+        if max_page == 1 :
+            st.sidebar.write("全" + str(len(st.session_state.filtered_df)) + "件を表示します")
+
+        else :
+            st.session_state.display_page = st.sidebar.slider("表示するページ", 1, max_page, 1)
+            
+        # 一度に表示する分のデータを抽出
+        displayed_df = display_df(st.session_state.filtered_df, st.session_state.display_number ,st.session_state.display_page )
+
+        # 一覧表示
+        st.write(displayed_df)
+
+        # 指定した物件について詳細な情報を表示する
+        selected_one = st.radio("詳しく知りたい物件の番号を選択してください", displayed_df.index, horizontal=True)
+
+        # 直接リンクを作成
+        st.write(st.session_state.filtered_df.iloc[selected_one]["URL"])
+
+        # foliumを使って地図を表示
+        display_map(st.session_state.filtered_df, displayed_df, selected_one)
+
+
+
